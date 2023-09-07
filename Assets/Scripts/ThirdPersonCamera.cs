6ca00e4acc740d823a17b4b8328f954e767a43b0
isaiahmbernal <isaiahmbernal@gmail.com>
2023-09-07 13:20:28 -0500

    :construction: Combat Camera

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -10,37 +10,88 @@ public class ThirdPersonCamera : MonoBehaviour
     [SerializeField] private Transform playerObj;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private PlayerMovement pm;
+    [SerializeField] private Transform combatLookAt;
+    [SerializeField] private GameObject basicCam;
+    [SerializeField] private GameObject combatCam;
 
     [Header("Variables")]
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private CameraStyle cameraStyle;
+
+    [Header("Keybinds")]
+    [SerializeField] private KeyCode aimKey = KeyCode.Mouse1;
+
+    private enum CameraStyle {
+        Basic,
+        Combat
+    }
 
     public Transform getOrientation() {
+
         return orientation;
+
     }
 
-    void Start()
-    {
+    void Start() {
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
     }
 
-    void Update()
-    {
+    void Update() {
+
         // Rotate Orientation
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        if (pm.getCanMove()) {
-            // Rotate Player Object
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (Input.GetKeyDown(aimKey)) {
+            SwitchCameraStyle(CameraStyle.Combat);
+        } else if (Input.GetKeyUp(aimKey)) {
+            SwitchCameraStyle(CameraStyle.Basic);
+        }
 
-            if (inputDir != Vector3.zero) {
-                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        // If Camera Style Basic
+        if (cameraStyle == CameraStyle.Basic) {
+
+            if (pm.getCanMove()) {
+
+                // Rotate Player Object
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
+                Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+                if (inputDir != Vector3.zero) {
+
+                    playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+
+                }
+
             }
+
+        // If Camera Style Combat
+        } else if (cameraStyle == CameraStyle.Combat) {
+
+            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+
+            playerObj.forward = dirToCombatLookAt.normalized;
+
         }
         
+    }
+
+    private void SwitchCameraStyle(CameraStyle newStyle)
+    {
+
+        combatCam.SetActive(false);
+        basicCam.SetActive(false);
+
+        if (newStyle == CameraStyle.Basic) basicCam.SetActive(true);
+        if (newStyle == CameraStyle.Combat) combatCam.SetActive(true);
+
+        cameraStyle = newStyle;
+
     }
 
 }
